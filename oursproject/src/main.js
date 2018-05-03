4e13a2b286d7b36460d440b4b190ed65a0b96e6a
Niublity <38355384+Niublity@users.noreply.github.com>
2018-05-03 21:42:25 +0800

    公共内容

--- oursproject/src/main.js
+++ oursproject/src/main.js
@@ -3,13 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import Vuex from "vuex"
+//引入elementui
+import ElementUI from "element-ui"
+import 'element-ui/lib/theme-chalk/index.css';
+//引入axios
+import axios from "axios"
+import VueAxios from "vue-axios"
 Vue.config.productionTip = false
-
+Vue.use(VueAxios, axios)
+Vue.use(ElementUI);
+Vue.use(Vuex)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
