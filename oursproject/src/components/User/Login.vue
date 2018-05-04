4d5f408ed3bda93e62460e2e7999b708072a142b
liangmengli <329378530@qq.com>
2018-05-04 20:43:09 +0800

    no message

---
+++ oursproject/src/components/User/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      登录注册界面
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- oursproject/src/router/index.js
+++ oursproject/src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import CityList from "@/components/Locations/CityLIst"
-
+import SearchHistory from "@/components/Locations/SearchHistory"
+import Login from "@/components/User/Login"
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +11,17 @@ export default new Router({
       path: '/CityList',
       name: 'CityList',
       component: CityList
-    }
+    },
+    {
+      path: '/city/:id',
+      name: 'SearchHistory',
+      component: SearchHistory
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+
   ]
 })
