a0e1da553bd0cd111dff9d833a6d96a7b01a9203
DESKTOP-UFBNF6S\32937 <329378530@qq.com>
2018-05-18 11:09:25 +0800

    no message

---
+++ oursproject/src/page/shopcar/orderChilds/searchAddress.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Header :title="title"></Header>
+    </div>
+</template>
+
+<script>
+import Header from "../../../components/foodheader/foodheader";
+export default {
+    name:"searchAddress",
+    data() {
+        return {
+            title:"搜索"
+        };
+    },
+    components: {
+        Header
+    }
+};
+</script>
+<style lang='scss' scoped>
+</style>
